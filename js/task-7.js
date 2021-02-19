8165fb6185ffb93d66673df5f560425a70b647a2
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-19 23:21:41 +0200

    Task-7

--- js/task-7.js
+++ js/task-7.js
@@ -5,21 +5,43 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 /*
 
-Задание
-Объяви переменную fruitsArrayLength и присвой ей длину массива fruits используя свойство length.
+Добавь объекту apartment несколько новых свойств:
 
+area - площадь в квадратных метрах, число 60;
+rooms - количество комнат, число 3;
+location - местоположение квартиры, обьект со следующими вложенными свойствами;
+country - страна, строка 'Ямайка';
+city - город, строка 'Кингстон'.
 Тесты
-Объявлена переменная fruitsArrayLength.
-Значение переменной fruitsArrayLength это число 4.
+Объявлена переменная apartment.
+Значение переменной apartment это объект.
+Значение вложенного свойства area это число 60.
+Значение вложенного свойства rooms это число 3.
+Значение вложенного свойства location это объект.
+Значение вложенного свойства location.country это строка 'Ямайка'.
+Значение вложенного свойства location.city это строка 'Кингстон'.
 
 */
 
 //Task START
 
-// Стартовый код
-const fruits = ['яблоко', 'персик', 'груша', 'банан'];
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Просторная квартира в центре',
+  rating: 4.7,
+  price: 5000,
+  tags: ['premium', 'promoted', 'top', 'trusted'],
+  owner: {
+    name: 'Генри Сибола',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com'
+  }
+};
+
 // Пиши код ниже этой строки
-const fruitsArrayLength = fruits.length;
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location = {country: 'Ямайка', city: 'Кингстон'}; 
 
 //Task END
 
