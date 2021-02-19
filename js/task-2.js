6120584780e7eed84bd5a9e3084ee07c84b11ccc
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-19 22:19:58 +0200

    Task-2

--- js/task-2.js
+++ js/task-2.js
@@ -5,38 +5,28 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 /*
 
-Задание
-Функция checkPassword получает пароль пользователя в параметр password, проверяет его на совпадение с паролем администратора в переменной ADMIN_PASSWORD и возвращает сообщение о результате сравнения.
+Дополни объект квартиры свойством owner, значением которого будет объект с информацией о владельце. Добавь ему следующие свойства:
 
-Проведи рефакторинг кода функции checkStorage используя паттерн «ранний возврат»:
-
-удали переменную message;
-удали else;
-код должен работать так же, как и до оптимизации.
-Тесты
-Объявлена функция checkPassword(password).
-Вызов checkPassword('mangohackzor') возвращает 'Доступ запрещен, неверный пароль!'.
-Вызов checkPassword('polyhax') возвращает 'Доступ запрещен, неверный пароль!'.
-Вызов checkPassword('jqueryismyjam') возвращает 'Добро пожаловать!'.
+name - имя владельца, значение 'Генри';
+phone - телефон, значение '982-126-1588';
+email - почта, значение 'henry.carter@aptmail.com'.
 
 */
 
 //Task START
 
-function checkPassword(password) {
-  const ADMIN_PASSWORD = 'jqueryismyjam';
-  // Пиши код ниже этой строки
-  
-
-  if (password === ADMIN_PASSWORD) {
-   return 'Добро пожаловать!';
-  } 
-   return 'Доступ запрещен, неверный пароль!';
-  
-
-  
-  // Пиши код выше этой строки
-}
+const apartment = {
+    imgUrl: 'https://via.placeholder.com/640x480',
+    descr: 'Просторная квартира в центре',
+    rating: 4,
+    price: 2153,
+    tags: ['premium', 'promoted', 'top'],
+  owner:{
+    name: 'Генри',
+    phone:  '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  }
+  };
 
 
 //Task END
