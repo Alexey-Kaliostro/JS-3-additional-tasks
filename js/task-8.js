51dba8066871c543d22f93930c1b85220d2abbd3
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-19 23:40:09 +0200

    task-8

--- js/task-8.js
+++ js/task-8.js
@@ -5,26 +5,35 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 /*
 
-Задание
-Объяви две перемнные:
+Дополни код объявления объекта так, чтобы у него были свойства name, price, image и tags со значениями из переменных с аналогичными именами. Обязательно используй синтаксис коротких свойств.
 
-Имя переменной	Ожидаемое значение
-lastElementIndex	индекс последнего элемента масcива fruits через длина_массива - 1
-lastElement	значение последнего элемента массива
 Тесты
-Объявлена переменная lastElementIndex.
-Значение переменной lastElementIndex это число 3.
-Объявлена переменная lastElement.
+Объявлена переменная product.
+Значение переменной product это объект.
+Значение вложенного свойства name это строка 'Ремонтный дроид'.
+Значение вложенного свойства price это число 2500.
+Значение вложенного свойства image это строка 'https://via.placeholder.com/640x480'.
+Значение вложенного свойства tags это массив ['on sale', 'trending', 'best buy'].
 
 */
 
 //Task START
 
-// Стартовый код
-const fruits = ['яблоко', 'персик', 'груша', 'банан'];
-// Пиши код ниже этой строки
-const lastElementIndex = fruits.length - 1;
-const lastElement = fruits[lastElementIndex];
+const name = 'Ремонтный дроид';
+const price = 2500;
+const image = 'https://via.placeholder.com/640x480';
+const tags = ['on sale', 'trending', 'best buy'];
+
+const product = {
+  // Пиши код ниже этой строки
+
+    name,
+  price,
+  image,
+  tags,
+
+  // Пиши код выше этой строки
+};
 
 //Task END
 
