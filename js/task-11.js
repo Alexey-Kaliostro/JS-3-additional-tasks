4d70c9d99b5c74891b546e42e8a0028ae582fef2
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-03-19 00:16:58 +0200

    task-11

--- js/task-11.js
+++ js/task-11.js
@@ -6,30 +6,43 @@ console.log(" ");
 /*
 
 Задание
-Сервису гравировки украшений нужна функция, которая бы автоматически считала цену гравировки, в зависимости от количества слов и цены за слово.
-
-Объявлена функция calculateEngravingPrice(message, pricePerWord). Эта функция принимает строку, состоящую из слов разделённых только пробелами (параметр message) и цену гравировки одного слова (параметр pricePerWord).
-
-Напиши тело функции, чтобы она возвращала общую стоимость гравировки всех слов в строке.
+Выполни рефакторинг решения предыдущего задания добавив в цикл for...in проверку на собственное свойство.
 
 Тесты
-Объявлена функция calculateEngravingPrice(message, pricePerWord).
-Вызов calculateEngravingPrice('JavaScript у меня в крови', 10) возвращает 50.
-Вызов calculateEngravingPrice('JavaScript у меня в крови', 20) возвращает 100.
-Вызов calculateEngravingPrice('Веб-разработка это творческая работа', 40) возвращает 160.
-Вызов calculateEngravingPrice('Веб-разработка это творческая работа', 20) возвращает 80.
+Объявлена переменная advert.
+Значение переменной advert это объект.
+Объявлена переменная apartment.
+Значение переменной apartment это объект.
+Объявлена переменная keys.
+Значение переменной keys это массив ['descr', 'rating', 'price'].
+Объявлена переменная values.
+Значение переменной values это массив ['Просторная квартира в центре', 4, 2153].
 
 */
 
 //Task START
 
-function calculateEngravingPrice(message, pricePerWord) {
+const keys = [];
+const values = [];
+const advert = {
+  service: 'apt',
+};
+const apartment = Object.create(advert);
+apartment.descr = 'Просторная квартира в центре';
+apartment.rating = 4;
+apartment.price = 2153;
+
+for (const key in apartment) {
   // Пиши код ниже этой строки
-
-return message.split(' ').length * pricePerWord;
-
+  
+if (apartment.hasOwnProperty(key)){
+  keys.push(key);
+  values.push(apartment[key]);
+}
+  
   // Пиши код выше этой строки
 }
+
 
 //Task END
 
