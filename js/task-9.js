5b0ab3a7bb4bc49d1550b4e83854079e7ddb43cc
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-03-18 23:43:46 +0200

    task-9

--- js/task-9.js
+++ js/task-9.js
@@ -5,27 +5,33 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 /*
 
-Задание
-Напиши функцию getExtremeElements(array) которая принимает один параметр array - массив элементов произвольной длины. Функция должна возвращать массив из двух элементов - первого и последнего элемента параметра array.
+Дополни код объявления объекта credentials так, чтобы в результате у него были два свойства: email и password, имена которых хранятся в переменных emailInputName и passwordInputName.
+
+Значением свойства email должна быть строка 'henry.carter@aptmail.com', а значением свойства password - строка 'jqueryismyjam'.
 
 Тесты
-Объявлена функция getExtremeElements(array).
-Вызов getExtremeElements([1, 2, 3, 4, 5]) возвращает [1, 5].
-Вызов getExtremeElements(['Земля', 'Марс', 'Венера']) возвращает ['Земля', 'Венера'].
-Вызов getExtremeElements(['яблоко', 'персик', 'груша', 'банан']) возвращает ['яблоко', 'банан'].
+Объявлена переменная credentials.
+Значение переменной credentials это объект.
+В объекте credentials есть свойство email.
+Значение вложенного свойства email это строка 'henry.carter@aptmail.com'.
+В объекте credentials есть свойство password.
+Значение вложенного свойства password это строка 'jqueryismyjam'.
 
 */
 
 //Task START
 
-function getExtremeElements(array) {
-    // Пиши код ниже этой строки
-    
- return  [array[0], array[array.length -1]]
+const emailInputName = 'email';
+const passwordInputName = 'password';
 
-
-    // Пиши код выше этой строки
-  }
+const credentials = {
+  // Пиши код ниже этой строки
+  [emailInputName]: 'henry.carter@aptmail.com',
+  [passwordInputName]: 'jqueryismyjam',
+  
+  
+  // Пиши код выше этой строки
+};
 
 //Task END
 
